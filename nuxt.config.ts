f0cd076efa194a3256faafebabe91eec70c53cca
Dominic <debidedominic@gmail.com>
2024-04-30 10:42:50 +0800

    Fix issue on app aliases.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,4 +18,23 @@ export default defineNuxtConfig({
       API_URL: 'https://12e4d0vdb9.execute-api.us-east-1.amazonaws.com/sandbox'
     },
   },
+  appConfig: {
+    nuxtIcon: {
+      size: '1.50em',
+      aliases: {
+          person_circle: 'mdi:person-circle',
+          bell: 'mdi:bell',
+          close: 'mdi:close',
+          plus: 'mdi:add',
+          calendar: 'mdi:calendar-month',
+          email_outline: 'mdi:email-outline',
+          trash_outline: 'mdi:trash-can-outline',
+          pencil_fill: 'ph:pencil-simple-line-duotone',
+          carret_down: 'mingcute:down-line',
+          carret_down_fill: 'mingcute:down-small-fill',
+          carret_next: 'mdi:chevron-right',
+          carret_back: 'mdi:chevron-left'
+      }
+    }
+  }
 })
